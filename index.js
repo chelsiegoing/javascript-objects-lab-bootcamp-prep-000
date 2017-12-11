f12364325eb4a5bea6d9a7210c8c07ed18f3924e
Cas <lydiahamil@yahoo.com>
2017-12-11 06:03:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {
+  breakfast: 'oat',
+  lunch: 'egg'
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+ return Object.assign({}, object, {[key]: value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(recipes, food, mcdonalds) {
+  recipes[food] = mcdonalds;
+  return recipes
+}
+function deleteFromObjectByKey(food, mcdonalds) {
+  delete recipes[food];
+  return recipes
+}
